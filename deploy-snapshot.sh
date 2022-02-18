e54bf098f3bc52b9326e48e372f012e73433363d
i-Taozi <879714748@qq.com>
2022-02-19 00:07:42 +0800

    dimovelev/metrics-sampler:original code

--- deploy-snapshot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mvn -T 1C clean gpg:sign deploy -Dgpg.executable=gpg2 -s ~/.m2/settings.xml
-
--- jsondoc-core/src/test/java/org/jsondoc/core/util/pojo/ClassWithConstant.java
+++
@@ -1,9 +0,0 @@
-package org.jsondoc.core.util.pojo;
-
-public class ClassWithConstant {
-
-    public static final String THIS_IS_A_CONSTANT = "test";
-
-    private String identifier;
-
-}
